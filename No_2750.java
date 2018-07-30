8af8b62b870d26b5353c0a9517142dbc0ada2e2b
NamJinWoo <cjsencks@naver.com>
2018-07-30 20:37:47 +0900

    sort

--- No_2750.java
+++ No_2750.java
@@ -1,13 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class No_2750 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
 		int[] arr = new int[num];
 		int i = 0;
 		while(i<num) {
-			arr[i] = sc.nextInt();
+			arr[i] = Integer.parseInt(br.readLine());
 			i++;
 		}
 		Arrays.sort(arr);
