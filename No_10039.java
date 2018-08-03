c47992bdeae54a3fdedd6404c7bdf35b2acb9a29
NamJinWoo <cjsencks@naver.com>
2018-08-03 18:17:02 +0900

    get avg

---
+++ No_10039.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class No_10039 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = 0;
+		int sum = 0;
+		while(i<5) {
+			int num = sc.nextInt();
+			if(num < 40) {
+				sum +=40;
+			}else {
+				sum += num;
+			}
+			i++;
+		}
+		System.out.println(sum/5);
+	}
+}
