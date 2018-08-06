187086f6c72712ec4383562ed1bd23cb749bf069
NamJinWoo <cjsencks@naver.com>
2018-08-06 14:31:19 +0900

    read string

---
+++ No_2908.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class No_2908 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int num1 = Integer.parseInt(st.nextToken());
+		int num2 = Integer.parseInt(st.nextToken());
+		int newNum1 = (num1 % 10) * 100 + ((num1 / 10) % 10) * 10 + num1 / 100;
+		int newNum2 = (num2 % 10) * 100 + ((num2 / 10) % 10) * 10 + num2 / 100;
+		
+		if(newNum1 > newNum2) {
+			bw.write(newNum1+"");
+		}else {
+			bw.write(newNum2+"");
+		}
+		bw.close();
+	}
+}
