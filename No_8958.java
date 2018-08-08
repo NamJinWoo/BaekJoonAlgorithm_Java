deb73607d6df56115b6fa11edeb0ec56b8aff9dc
NamJinWoo <cjsencks@naver.com>
2018-08-08 20:35:31 +0900

    덜함..

--- No_8958.java
+++ No_8958.java
@@ -1,18 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class No_8958 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int num = Integer.parseInt(br.readLine());
 		int i = 0;
 		String[] arr = new String[num];
 		while (i < num) {
-			String result = sc.next();
-			arr[i] = result;
+			arr[i] = br.readLine();
 			i++;
 		}
 
-		
 		int k;
 		for (k = 0; k < arr.length; k++) {
 			int score = 1, sum = 0;
@@ -29,8 +33,8 @@ public class No_8958 {
 				}
 			}
 
-			System.out.println(sum);
+			bw.write(sum+"\n");
 		}
-
+		bw.close();
 	}
 }
