9a9c20198a059d6f0efb78824aca725c8bc14560
NamJinWoo <cjsencks@naver.com>
2018-08-11 18:42:08 +0900

    group word checker

---
+++ No_1316.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class No_1316 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int num = sc.nextInt();
+		String[] word = new String[num];
+		int count = 0;
+		for(int i = 0; i< num ; i++) {
+			word[i] = sc.nextLine();
+		}//단어들을 배열에 저장한다.
+		
+		
+	}
+}
