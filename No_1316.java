16ffb5614d48fbb27cfc11296a5b5853ed0e34b2
NamJinWoo <cjsencks@naver.com>
2018-08-11 19:04:46 +0900

    그룹 단어 체커

--- No_1316.java
+++ No_1316.java
@@ -4,14 +4,25 @@ import java.util.Scanner;
 
 public class No_1316 {
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		String[] word = new String[num];
-		int count = 0;
-		for(int i = 0; i< num ; i++) {
-			word[i] = sc.nextLine();
-		}//단어들을 배열에 저장한다.
+		int count = num;
+
+		for (int i = 0; i < num; i++) {
+			String x = sc.next();
+			boolean[] checker = new boolean[26];// boolean배열의 기본값은 false임
+
+			for (int j = 1; j < x.length(); j++) {
+				if (x.charAt(j - 1) != x.charAt(j)) {
+					if (checker[x.charAt(j) - 97] == true) {
+						count--;
+						break;
+					}
+					checker[x.charAt(j - 1) - 97] = true;
+				}
+			}
+		}
 		
-		
+		System.out.println(count);
 	}
 }
