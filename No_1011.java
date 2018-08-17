c57f86d4b149ceb0a5fb84d066070b008828c2dc
NamJinWoo <cjsencks@naver.com>
2018-08-17 18:32:31 +0900

    1011

--- No_1011.java
+++ No_1011.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
 
 public class No_1011 {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int i = 0;
+		while(i<num) {
+			String x = sc.nextLine();
+			i++;
+		}
+	}
 }
