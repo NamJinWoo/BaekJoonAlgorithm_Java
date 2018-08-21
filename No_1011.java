e1d367a1cd0e7587448b03e8e9153e4c9474a483
NamJinWoo <cjsencks@naver.com>
2018-08-21 22:10:33 +0900

    Fly me to the Alpha Centauri

--- No_1011.java
+++ No_1011.java
@@ -1,43 +1,47 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class No_1011 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int j = 0;
 		int c = Integer.parseInt(br.readLine());
-		int x, y, n, sum, k, count;
+		long x, y, n, k, count;
 		No_1011 a = new No_1011();
 		while (j < c) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			x = Integer.parseInt(st.nextToken());
 			y = Integer.parseInt(st.nextToken());
+			if(y-x == 1) {
+				System.out.println("1");
+				j++;
+				continue;
+			}else if(y-x == 0) {
+				System.out.println("0");
+				j++;
+				continue;
+			}
 
 			n = a.findN(x, y);
-			sum = n * (n + 1) / 2;
-			k = y - x - sum * 2;
+			k = y - x - n * (n + 1);
 			count = n * 2;
 
 			if (k == 0) {
 				count = count + 0;
-			} else if (k <= n + 1 && k > 0) {
+			} else if (k <= n + 1) {
 				count = count + 1;
 			} else {
 				count = count + 2;
 			}
-			bw.write(count + "\n");
+			System.out.println(count);
 			j++;
 		}
-		bw.close();
 	}
 
-	public int findN(int x, int y) {
-		int i = 1;
+	public long findN(long x, long y) {
+		long i = 1;
 		while (true) {
 			if ((i + 1) * (i + 2) > y - x) {
 				break;
